rotation fixes and added animation subsampling_keep_all

animation.py
@@ -66,6 +66,16 @@ class AnimationClip:
         return AnimationClip(self.root_positions[::step], self.rotations[::step], self.skeleton,
                              step * self.frame_time, positions).copy()
 
+    def subsample_keep_all(self, step=2):
+        anims = []
+        for s in range(step):
+            positions = {}
+            for jt in self.positions:
+                positions[jt] = self.positions[jt][s::step]
+            anims.append(AnimationClip(self.root_positions[s::step], self.rotations[s::step], self.skeleton,
+                                       step * self.frame_time, positions).copy())
+        return anims
+
     def mirror(self):
         self.skeleton.generate_mir_map()
         mir_map = self.skeleton.mir_map
@@ -189,7 +199,7 @@ class AnimationClip:
 
         # Transfer yaw rotation
         old_root_rots = self.rotations[:, 0].copy()
-        ups_tf, fwds_tf = old_root_rots.two_axis()
+        ups_tf, fwds_tf = rotation.quat_to_two_axis(old_root_rots)
         ups_tf = np.zeros_like(ups_tf)
         ups_tf[..., 1] = 1.0
         fwds_tf[..., 1] = 0.0

rotation.py
@@ -324,7 +324,7 @@ def two_axis_to_quat(ups_tf, fwds_tf, up_idx=1, fwd_idx=2):
     assert x_idx is not None
 
     ups_tf = ups_tf / np.linalg.norm(ups_tf, axis=-1)[..., None]
-    fwds_tf = fwds_tf - np.sum(ups_tf * fwds_tf, axis=-1) * ups_tf
+    fwds_tf = fwds_tf - np.sum(ups_tf * fwds_tf, axis=-1)[..., None] * ups_tf
     cross = np.cross(ups_tf, fwds_tf, axis=-1)
 
     m = np.empty(ups_tf.shape[:-1] + (3, 3))